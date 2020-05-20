887b82ef0c9b27b865a275aa131561a8bafbbc6d
Ubuntu <su2tech@vps-b5cb32ab.vps.ovh.net>
2020-05-20 18:02:15 +0000

    initialisation

---
+++ packages/firewall.sh
@@ -0,0 +1,6 @@
+#!/scbin/sh
+echo "UFW : SART INSTALLATION"
+sudo apt install ufw >> $installation_log
+
+
+
---
+++ packages/server_lamp.sh
@@ -0,0 +1,5 @@
+#/sbin/bash
+echo "****LAMP : START INSTALLATION****"
+
+
+echo "****LAMP : START CONFIGURATION****"
